eb8fd007330b3978e3feee0cc839772884b198ab
李小东 <1658011573@qq.com>
2026-02-25 00:47:01 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -188,8 +188,8 @@ else
     exit 1
 fi
 
-echo "Generating [out/arch/arm64/boot/dtb]......"
-find out/arch/arm64/boot/dts -name '*.dtb' -exec cat {} + >out/arch/arm64/boot/dtb
+#echo "Generating [out/arch/arm64/boot/dtb]......"
+#find out/arch/arm64/boot/dts -name '*.dtb' -exec cat {} + >out/arch/arm64/boot/dtb
 
 
 
