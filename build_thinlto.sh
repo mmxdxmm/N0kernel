f2640098d3dc133100611366525775cc1b79d58d
李小东 <1658011573@qq.com>
2026-02-24 18:35:48 +0800

    Add files via upload

--- build_thinlto.sh
+++ build_thinlto.sh
@@ -129,7 +129,7 @@ rm -rf out/
 #更新所有文件的时间戳为系统时间
 find . -exec touch -h {} +
 
-make KCFLAGS="-mcpu=cortex-a77" KBUILD_CFLAGS="-mcpu=cortex-a77" $MAKE_ARGS ${TARGET_DEVICE}_defconfig
+make KCFLAGS="-mcpu=cortex-a55" KBUILD_CFLAGS="-mcpu=cortex-a55" $MAKE_ARGS ${TARGET_DEVICE}_defconfig
 
 if [ $KSU_ENABLE -eq 1 ]; then
     scripts/config --file out/.config \
@@ -179,7 +179,7 @@ scripts/config --file out/.config \
     -e CONFIG_KALLSYMS_ALL \
     -e CONFIG_LD_DEAD_CODE_DATA_ELIMINATION
 
-make KCFLAGS="-mcpu=cortex-a77" KBUILD_CFLAGS="-mcpu=cortex-a77" $MAKE_ARGS -j$(nproc) Image.lz4 dtbs
+make KCFLAGS="-mcpu=cortex-a55" KBUILD_CFLAGS="-mcpu=cortex-a55" $MAKE_ARGS -j$(nproc) Image.lz4 dtbs
 
 
 
